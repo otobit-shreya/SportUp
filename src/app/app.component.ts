782f5019bfca5cc263c21b6079300071451d5474
otobit-kajal <kajal@otobit.in>
2023-12-21 17:59:18 +0530

    change main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MainComponent } from './main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
