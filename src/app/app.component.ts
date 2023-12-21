3dbe5b23bd8c796addc7df8b94b5f754301d8673
otobit-shreya <arshreya@otobit.in>
2023-12-21 18:13:43 +0530

    change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { VerifydelaccComponent } from './verifydelacc/verifydelacc.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,MainComponent],
+  imports: [CommonModule, RouterOutlet,MainComponent, VerifydelaccComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
