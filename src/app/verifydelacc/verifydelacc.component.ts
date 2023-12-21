8ba2e0e2a7cc3e53a097f68f8ad5f3c6956c7b86
Shreya Arande <arshreya@otobit.in>
2023-12-21 18:08:01 +0530

    Verify del

---
+++ src/app/verifydelacc/verifydelacc.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verifydelacc',
+  standalone: true,
+  imports: [],
+  templateUrl: './verifydelacc.component.html',
+  styleUrl: './verifydelacc.component.css'
+})
+export class VerifydelaccComponent {
+
+}
