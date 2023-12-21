ab5906d0fce06789945617e9fca4c3017c98a8fd
otobit-shreya <arshreya@otobit.in>
2023-12-21 19:42:19 +0530

    del confirm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { VerifydelaccComponent } from './verifydelacc/verifydelacc.component';
+import { DelconfirmComponent } from './delconfirm/delconfirm.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,MainComponent, VerifydelaccComponent],
+  imports: [CommonModule, RouterOutlet,MainComponent, VerifydelaccComponent, DelconfirmComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/delconfirm/delconfirm.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delconfirm',
+  standalone: true,
+  imports: [],
+  templateUrl: './delconfirm.component.html',
+  styleUrl: './delconfirm.component.css'
+})
+export class DelconfirmComponent {
+
+}
